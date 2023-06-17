497ce7febf05ef48e2a2fe70711340c0d52b7f4b
YellowFlash <133869245+TheRealYellowFlash@users.noreply.github.com>
2023-06-17 23:37:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,8 @@ def gdtotv3(url):
           res = cget("GET",f"{p_url.scheme}://{p_url.hostname}/ddl/{url.split('/')[-1]}")
       except Exception as e:
           raise DirectDownloadLinkException(f'ERROR: {e.__class__.__name__}')
-      if (drive_link := findall(r"myDl\('(.*?)'\)", res.text)) and "drive.google.com" in drive_link[0]:
+      drive_link = re.findall(r"myDl\('(.*?)'\)", res.text)
+      if drive_link and "drive.google.com" in drive_link[0]:
           return drive_link[0]
       else:
           raise DirectDownloadLinkException('ERROR: Drive Link not found, Try in your broswer')
